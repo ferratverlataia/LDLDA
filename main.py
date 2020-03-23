c6eabd8f5e0bbfaf0ac2770aea10d0274140eb8e
cubomx <cheservelram657>
2020-03-22 20:32:46 -0600

    Already reading all file
    Signed-off-by: cubomx <cheservelram657>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Segmento import Segmento
 import LDLDA
 BREAKLINE = 4
 
-def createVert(file):
+def createLDLA(file):
     vert = open(file + ".ver", "r").read().split("\n")
     vertices = dict()
     for idx, el in enumerate(vert):
@@ -19,7 +19,6 @@ def createVert(file):
     for x, el in enumerate(ari):
         if x >= 4:
             info = el.split()
-            print(info)
             if len(info) > 1:
                 origen = vertices[info[1]]
                 aristas[info[0]] = LDLDA.arista(origen)
@@ -30,27 +29,38 @@ def createVert(file):
     for y, el in enumerate(car):
         if y >= 4:
             info = el.split()
-            print(info)
             if len(info) > 1:
-                interno, externo = None, None
-                if info[1] in aristas:
-                    nueva_car.cmpint(aristas[info[1]])
-                if info[2] in aristas:
-                    nueva_car.cmpext(aristas[info[2]])
                 nueva_car = LDLDA.cara()
+
+                if info[1] in aristas:
+                    nueva_car.cmpint.append(aristas[info[1]])
+                if info[2] in aristas:
+                    nueva_car.cmpext.append(aristas[info[2]])
 
                 caras[info[0]] = nueva_car
 
+    for idx, el in enumerate(vert):
+        if idx >= 4:
+            info = el.split()
+            if len(info) > 1:
+                actual = vertices[info[0]]
+                actual.aristaincidente.append(aristas[info[3]])
 
-
-def example01():
-    # face 1
-    ver01 = createVert("layer01")
-
+    for idx, el in enumerate(aristas):
+        if idx >= 4:
+            info = el.split()
+            if len(info) > 1:
+                actual = aristas[info[0]]
+                actual.Pareja = aristas[info[2]]
+                actual.caraincidente = caras[info[3]]
+                actual.siguiente = aristas[info[4]]
+                actual.siguiente = aristas[info[5]]
 
 
 def main():
-    example01()
+    ver01 = createLDLA("layer01")
+    ver02 = createLDLA("layer02")
+
     '''  
     
     segmentos = [s1,s2]
